c2f614344bc75087f17346a890cd4f446a2411ac
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2022-06-15 14:53:00 +0100

    Add files via upload

--- acknowledge.js
+++ acknowledge.js
@@ -1,21 +1,65 @@
-document.querySelectorAll(".carousel").forEach(carousel => {
+const extraText = document.querySelector(".extra-text")
+const extraText1 = document.querySelector(".extra-text1")
+const extraText2 = document.querySelector(".extra-text2")
+const extraText3 = document.querySelector(".extra-text3")
+const extraText4 = document.querySelector(".extra-text4")
+const extraText5 = document.querySelector(".extra-text5")
+const extraText6 = document.querySelector(".extra-text6")
+const extraText7 = document.querySelector(".extra-text7")
 
-    const items = carousel.querySelectorAll(".carousel-item");
-    const buttonHtml = Array.from(items, () => {
-        return`<span class='carousel-button'></span>`
-    });
-    carousel.insertAdjacentHTML('afterend', `
-    <div class= 'carousel-nav'>
-    ${buttonHtml.join('')}
-    </div>
-    `);
-    const buttons = carousel.querySelectorAll(".carousel-button");
+function openModal() {
+  extraText.showModal();
+}
+function closeModal() {
+  extraText.close();
+}
 
-    buttons.forEach((button, i) => (
-        button.addEventListener('click', () => {
-        items.forEach(item => item.classList.remove('carousel-item-selected'))   
-        buttons.forEach(button => button.classList.remove('carousel-button-selected'))         
+function openModal1() {
+  extraText1.showModal();
+}
+function closeModal1() {
+  extraText1.close();
+}
 
-        })
-    ))
-});
+function openModal2() {
+  extraText2.showModal();
+}
+function closeModal2() {
+  extraText2.close();
+}
+
+function openModal3() {
+  extraText3.showModal();
+}
+function closeModal3() {
+  extraText3.close();
+}
+
+function openModal4() {
+  extraText4.showModal();
+}
+function closeModal4() {
+  extraText4.close();
+}
+
+function openModal5() {
+  extraText5.showModal();
+}
+function closeModal5() {
+  extraText5.close();
+}
+
+function openModal6() {
+  extraText6.showModal();
+}
+function closeModal6() {
+  extraText6.close();
+}
+
+function openModal7() {
+  extraText7.showModal();
+}
+function closeModal7() {
+  extraText7.close();
+}
+    
